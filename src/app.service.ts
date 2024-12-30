f937c2cb0b467c278c15f77ffa4c9b98b3126550
Rendell Lasola <rlasola@highlysucceed.com>
2024-12-31 07:02:15 +0800

    added docker config

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from Github Actions v4!!!!';
+    return 'Hello World from Github Actions v4 and from Azure App Service baby!!!!';
   }
 }
